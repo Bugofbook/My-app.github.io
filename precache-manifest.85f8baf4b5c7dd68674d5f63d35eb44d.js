943d5b7c146e2a6362a985fea7231f4c2309c2a8
bugofbook <bugofbook@gmail.com>
2019-07-25 22:09:13 +0800

    Updates

---
+++ precache-manifest.85f8baf4b5c7dd68674d5f63d35eb44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "486dca55adc6d569ffa2",
+    "url": "/My-app/static/js/runtime~main.486dca55.js"
+  },
+  {
+    "revision": "b766dc31404183f82a89",
+    "url": "/My-app/static/js/main.b766dc31.chunk.js"
+  },
+  {
+    "revision": "ac6fd41774d6c7655102",
+    "url": "/My-app/static/js/2.ac6fd417.chunk.js"
+  },
+  {
+    "revision": "b766dc31404183f82a89",
+    "url": "/My-app/static/css/main.a32dcb00.chunk.css"
+  },
+  {
+    "revision": "f62b5454fcb968213de7d8992cc30d31",
+    "url": "/My-app/index.html"
+  }
+];
