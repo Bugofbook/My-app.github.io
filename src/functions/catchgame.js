91ec8d2953d24dae530e00082e06d88b33605774
BugofBook <bugofbook@gmail.com>
2019-08-12 15:16:31 +0800

    testing

--- src/functions/catchgame.js
+++ src/functions/catchgame.js
@@ -9,6 +9,7 @@ export const calculationArrays = (chess ={}, squares = []) => {
 		if (vec === [0,0]) {
 			continue
 		}
+		//resultArray.concat(checkLine(vec)(1,1)(chess,squares))
 		for (let j = 1, jth = lengthx ; j < jth ; j ++) {
 			let tapointx = chess.rowskey + vec[0] * j
 			let tapointy = chess.columnskey + vec[1] * j
@@ -36,3 +37,44 @@ export const calculationArrays = (chess ={}, squares = []) => {
 	}
 	return resultArray
 }
+//call back array of chess which can catch 
+export const checkLine = (Vector = []) => (maxlength = 1 , interval = 1) => (CurrentPoint , NowSquares) => {
+	let newarray = takeLineChessFromSquares(Vector)(maxlength, interval)(CurrentPoint , NowSquares)
+//	console.log(newarray)
+	return checkCycleChessFromArray(newarray,CurrentPoint,[])
+}
+// take Array of chess from Square
+const takeLineChessFromSquares = (Vector = []) => (maxlength = 1 , interval = 1) => (CurrentPoint , NowSquares) => {
+	let resultarray = []
+	let lengthx = NowSquares.length
+	for (let i = 0 , ith = maxlength ; i < ith ; i++) {
+		let chessx = CurrentPoint.rowskey + Vector[0] * i * interval
+		let chessy = CurrentPoint.columnskey + Vector[1] * i * interval
+		if ( chessx >= 0 && chessx < lengthx && chessy >= 0 && chessy < NowSquares[chessx].length) { //check chess exist
+			let Nowsquare = NowSquares[chessx][chessy]
+			let NewChess = Object.assign(Nowsquare,{rowskey: chessx , columnskey: chessy }) 
+			resultarray.push(NewChess)
+		}
+		else {
+			break
+		}
+	}
+	return resultarray
+}
+//find result of catch-game from Array of chess
+const checkCycleChessFromArray = (checkarray,startchess,resultarray = []) => {
+	if ( checkarray.length === 0) {  // No square need check , return empty result
+		return []
+	}
+	let checksquare = checkarray.shift()
+	if (checksquare.value === startchess.value ) {  // find same chess , return result
+		return resultarray
+	}
+	else if (checksquare.value === "" ) { //find empty chess , return empty result
+		return []
+	}
+	else if (checksquare.value !==  startchess.value  ) { //No find same chess. continus find	
+		return checkCycleChessFromArray(checkarray,startchess,resultarray.concat([checksquare]))
+	}
+}
+
